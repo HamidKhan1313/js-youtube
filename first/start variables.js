fa0c8569f0b2e6d2e97cbe4999da7b5ee844afdb
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-09-21 14:45:30 +0000

    container config done

---
+++ first/start variables.js
@@ -0,0 +1,7 @@
+const accountid = 234445
+let accountemail = "Hamidkhantanol@gmail.com"
+var accountpassword = "12345678"
+accountcity = "Abbottabad"
+
+//accountid = 2 // not allowed
+console.log(accountid)
