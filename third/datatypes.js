fe97f2e9c7b2f6406900e259a6044d2e22cbbe90
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-09-23 05:20:06 +0000

    code space done

---
+++ third/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all code as a newer verstion of javascript
+/* alert(3+3) we use nodejs not browser*/ 
+console.log(3
+    +3) // code redebility should be high 
+console.log("Hamid");
+let state;
+console.log(state);
+
+
+let name = "Hamid";
+let age = 18;
+let isLoggedIn = true;
+
+
+
+// number  // => can be use upto 2 power upto 52 or 53
+// bigint // use for bigger number like in reddit 
+// string => ""
+// boolean => true/false
+// null // standalone value
+//undefined
+// symbol // mostly use in react / also a unique
+// object
+console.log(typeof isLoggedIn);
+console.log(typeof null); //null have type object
+console.log(typeof undefined); // undefined have type of undefined
+
