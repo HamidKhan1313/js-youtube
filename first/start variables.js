f52fc6f5a25c58426cda3998176f195bfb89401e
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-09-26 11:39:52 +0500

    Delete first directory

--- first/start variables.js
+++
@@ -1,7 +0,0 @@
-const accountid = 234445
-let accountemail = "Hamidkhantanol@gmail.com"
-var accountpassword = "12345678"
-accountcity = "Abbottabad"
-
-//accountid = 2 // not allowed
-console.log(accountid)
