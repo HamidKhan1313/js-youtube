37a6c811e259e1efb2ca831ea89a24c8aead48c7
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-09-26 06:37:15 +0000

    commit

---
+++ 03 DTC/datatypeconvertion.js
@@ -0,0 +1,8 @@
+let score = "33abc"
+
+console.log(typeof score)
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
