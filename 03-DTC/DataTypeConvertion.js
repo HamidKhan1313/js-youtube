edc72bba69c39e016e646f6dd812b7bddf596420
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-09-27 14:13:07 +0000

    javascript_solution

---
+++ 03-DTC/DataTypeConvertion.js
@@ -0,0 +1,11 @@
+//let score = "33abc"
+
+//console.log(typeof score)
+
+//let valueinnumber = Number(score)
+//console.log(typeof valueinnumber);
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
