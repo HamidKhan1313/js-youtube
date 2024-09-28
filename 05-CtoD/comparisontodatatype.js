28fb88d11e52ca4e978a15e11ab0c52b517bb410
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-09-28 14:34:01 +0000

    java code writen

---
+++ 05-CtoD/comparisontodatatype.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)         //  simple comparisons
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+// console.log("2" > 1)
+// console.log(2 > "1");
+
+// complex comparisons
+
+// console.log(null > 0);
+// console.log(null == 0);   // this line shows null is greater then 0 // in this null is converted 
+                         // to zero so this statement is read is null >= 0 become 0 >= 0 i.e true
+console.log(undefined == 0)
+console.log(undefined > 0);
+console.log(undefined < 0);  // undefined always give false value to 0 
+
+// === for check also datatype 
+console.log("2" === 2)  // "2" is string and 2 is number so it give false in answer
+// we give higher  preority to clean code in datatype (javascript) so we evoid these datatype such as null and undefined datatypes etc
+
+
