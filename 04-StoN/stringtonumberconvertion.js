f904ffb3025b1f9f4320c6fd227df540ea38f979
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-09-28 12:44:07 +0000

    java script code

--- 04-StoN/stringtonumberconvertion.js
+++ 04-StoN/stringtonumberconvertion.js
@@ -11,5 +11,32 @@ let negativenumber = -number
 let string1 = "hello"
 let string2 = "hamid"
 let string3 = string1 + string2
-console.log(string3);
+//console.log(string3);
+
+
+// console.log("2" + 1)
+// console.log(2 + "1");
+// console.log("2" + 1 + 1);   // if we write first string i.e 2 other code is also show AS "STRING"
+// console.log(2 + 1 + "1");
+//console.log(2 + 1 * 3 % 4);     // cannot write such a confution code in javascript
+
+// console.log(+true)   //  true give us a value => 1
+// console.log(+"")     //  "" give us a value => 0
+
+
+// prefix
+let gamechanger = 100
+++gamechanger 
+console.log(gamechanger);
+
+//postfix
+let gamechangerr = 100
+gamechangerr++
+console.log(gamechangerr);
+
+
+
+
+
+
 
