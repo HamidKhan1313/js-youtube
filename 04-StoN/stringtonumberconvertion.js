32605d75968ab42bc10448c1eaab828546c54182
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-09-28 12:32:58 +0000

    java code excuted

---
+++ 04-StoN/stringtonumberconvertion.js
@@ -0,0 +1,15 @@
+let number = 1
+let negativenumber = -number
+// console.log(negativenumber)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2)
+// console.log(2 / 2)
+
+let string1 = "hello"
+let string2 = "hamid"
+let string3 = string1 + string2
+console.log(string3);
+
