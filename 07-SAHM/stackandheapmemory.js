e46ec900a3be214f4fa694e81cc73665ec9e929e
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-10-01 15:15:14 +0000

    java script code

---
+++ 07-SAHM/stackandheapmemory.js
@@ -0,0 +1,8 @@
+// Stack (primitive ) heap (non-primitive)
+let myYoutubename = "HamidKhan"
+let anotherName =  myYoutubename
+anotherName = "chaiorcode"
+console.log(myYoutubename);
+
+console.log(anotherName);
+
