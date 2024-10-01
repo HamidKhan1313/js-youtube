baffbc27e6b152f9bee97f2899d839195014a8f3
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-10-01 15:34:28 +0000

    code run

---
+++ 06-summarydatatype/datatypesummary.js
@@ -0,0 +1,33 @@
+// primitive 
+ // 7 types : number, string, boolean, null, undefined , Symbol, BigInt
+
+const number = 100
+const numberval = 100.3
+const isLoggedIn = false
+const ousideTemp = null
+let userEmail ;
+console.log(userEmail);
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+const bigNumber = 22344558689045876849585758n   // n at last show us number big int
+
+//console.log(id === anotherId);  // it shows us data type of id and anotherid
+
+//console.log(bigNumber); 
+
+
+
+
+ // non primitive
+ // array , object, function
+
+ const heros = ["hamid", "danish", "badar ", "ammar"]
+const obj = {
+    name : "hamid",
+    age : 18 
+}
+
+console.log(typeof bigNumber);
+console.log(typeof ousideTemp);
+console.log(typeof isLoggedIn);
