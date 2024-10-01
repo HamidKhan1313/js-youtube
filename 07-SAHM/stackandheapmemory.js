d478acbec2f6a319d2f3557c35a37a8aa6625a61
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-10-01 15:33:13 +0000

    java code

--- 07-SAHM/stackandheapmemory.js
+++ 07-SAHM/stackandheapmemory.js
@@ -1,8 +1,16 @@
-// Stack (primitive ) heap (non-primitive)
+// Stack (primitive| it give copy  )  heap (non-primitive | it gives reference ) 
 let myYoutubename = "HamidKhan"
 let anotherName =  myYoutubename
 anotherName = "chaiorcode"
-console.log(myYoutubename);
+//console.log(myYoutubename);
 
-console.log(anotherName);
+//console.log(anotherName);
 
+let userOne = {
+    email : "user@gmail.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+userTwo.email = "Hamid@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);  // it updates both consoe.log value with the 2nd email ("hamid@gmail.com")
