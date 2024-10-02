cbf515e1a9f723d2490e34d8678dbf26a978efb6
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-10-02 04:52:45 +0000

    java code excuted

---
+++ 08-strings/strings.js
@@ -0,0 +1,7 @@
+const name = "Hamid"
+const repoCount = 50
+//console.log(name + repoCount + " value")
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+const gamename = new String ("Khan")
+c
