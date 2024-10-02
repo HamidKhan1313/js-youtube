12ae28fc07f6e20d9af5a18b9f8d8b6ec3d92fd8
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-10-02 06:26:37 +0000

    java script report

--- 08-strings/strings.js
+++ 08-strings/strings.js
@@ -3,5 +3,40 @@ const repoCount = 50
 //console.log(name + repoCount + " value")
 
 console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
-const gamename = new String ("Khan")
-c
+
+// another way to declare string
+
+const gameName = new String("Waleed-Khan")
+
+//console.log(gameName);
+
+
+//excess key value pairs
+//console.log(gameName[0])
+
+// excess prototype through method 2
+//console.log(gameName.__proto__);
+
+// use to find total length of string
+//console.log(gameName.length);
+
+// use to find the position of character it start from 0-17
+//
+//console.log(gameName.charAt(17));
+
+// use for convert all string to uppercase
+//console.log(gameName.toUpperCase());
+
+// for position find character also it start from (0) position so it consider from start Like
+// if we consider ("Hamid Khan Is King " we find indexof (i) it give us only 3)
+//console.log(gameName.indexOf('i'));
+
+
+// now first we insilize string with variabe and then convert into substring
+const newString = gameName.substring(0, 11)
+console.log(newString);
+
+// we give slice also negative value
+const anotherString = gameName.slice(-4 ,-1)
+console.log(anotherString);
+
