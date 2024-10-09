618481173d88652f7caa3d3fda2c759d29d094af
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-10-09 07:33:32 +0000

    java code excuted

---
+++ 14-json/objectdestructureandjson.js
@@ -0,0 +1,70 @@
+const course = {
+    coursename : "js in hindi",
+    price : 999,
+    courseinstructor : "Hitesh"
+
+}
+// course.courseinstructor
+
+// for single time excess course / we change courseinstructer to instructer for easily excess
+const{courseinstructor : instructer} = course
+//console.log(courseinstructor);
+console.log(instructer);
+
+// for destructring use in react
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "Hamid") 
+// console.log(navbar)
+
+// Api formate as "object" 
+// {
+//     "name": "Hamid",
+//     "courseName": "js in hindi",
+//     "price" : "150"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+{
+    "login": "hiteshchoudhary",
+    "id": 11613311,
+    "node_id": "MDQ6VXNlcjExNjEzMzEx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/hiteshchoudhary",
+    "html_url": "https://github.com/hiteshchoudhary",
+    "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+    "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+    "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+    "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+    "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+    "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Hitesh Choudhary",
+    "company": null,
+    "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+    "twitter_username": "hiteshdotcom",
+    "public_repos": 94,
+    "public_gists": 5,
+    "followers": 31928,
+    "following": 0,
+    "created_at": "2015-03-23T13:03:25Z",
+    "updated_at": "2024-09-28T18:38:57Z"
+  }
+
+
+
