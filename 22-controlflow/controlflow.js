87999f897c6f9fa983ded82d6593e178f46019e6
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-10-14 16:30:37 +0000

    javascript code excuted

---
+++ 22-controlflow/controlflow.js
@@ -0,0 +1,30 @@
+// emplement this condition
+if(true){
+
+}
+// not emplement line no 6, 7, and 8
+// if(false){
+
+// }
+
+const isLoggedin = true
+const temperature = 41
+
+// if(2 == "2"){
+// console.log("code Excuted");
+
+//  }
+ if(temperature === 41){
+    console.log("Less then 50");
+    
+     }
+     else{
+      console.log("temperature is greater than 50");
+      
+     }
+     console.log("Excuted");
+     
+
+// < , > , <= , >= , == , != , 
+// === is use for type check in above example we excute code it excuted but it is not possible that number is equal
+// to string thats why we use "===" instead of "=="
