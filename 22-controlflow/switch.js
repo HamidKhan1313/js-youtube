314bfe56b50bc2dc506a48820e4b34ac74cd5843
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-10-15 16:54:47 +0000

    javascript code excuted

--- 22-controlflow/switch.js
+++ 22-controlflow/switch.js
@@ -7,3 +7,53 @@
 //     default:
 //         break;
 // }
+
+
+//const month = 3                                     
+// switch (month) {
+//     case 1:
+//         console.log("janvuary");
+        
+//         break;
+//         case 2:
+//             console.log("fabrary");
+            
+//             break;
+//             // when condition matche we use break as a commit it excute all code except default
+//             case 3:
+//                 console.log("march");
+                
+//                // break;
+//                 case 4:
+//                 console.log("aprail");
+                
+//                 break;
+//     default:
+//         console.log("Default case match");
+        
+//         break;
+// }
+const month = "aprail"
+switch (month) {
+    case "jan":
+        console.log("jan");
+        
+        break;
+        case "feb":
+            console.log("fab");
+            
+            break;
+            // when condition matche we use break as a commit it excute all code except default
+            case "march":
+                console.log("march");
+                
+               // break;
+                case "aprail":
+                console.log("aprail");
+                
+                break;
+    default:
+        console.log("Default case match");
+        
+        break;
+}
