838f9df881f0488dd645826b53dbe9b37dc70adb
hamid-khan-13 <hamidkhantanol134@gmail.com>
2024-10-16 16:49:31 +0000

    javascript code excuted

---
+++ 23-iterations/whileloop.js
@@ -0,0 +1,19 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of lndex is ${index}`);
+//     index = index + 2
+// }
+
+// let mineArray = ['superman', 'spiderman', 'bateman', 'ironman']
+// let arr = 0
+// while (arr < mineArray.length) {
+//     console.log(`value is ${mineArray[arr]}`);
+//     arr = arr + 1
+// }
+
+  let score = 11  //let score = 1   // if score is 11 its directy print 11
+do {
+
+    console.log(`value of score is ${score}`);
+    score++
+} while (score <= 10);
