e9fb3abf5738954f699d14f68cca55d6d2d7d3f9
Hamid Khan <hamidkhantanol@gmail.com>
2024-10-19 06:17:48 +0000

    javascript code excuted

--- 24-HOL/highorderarrayloop.js
+++ 24-HOL/highorderarrayloop.js
@@ -1,3 +1,24 @@
+// in array we use "for-off" loop 
+// in objects we use "for-in" loop 
+
+
+// for off loop
+// const arr = [1, 2, 3, 4, 5]
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+
+// Map function
+//  const map = new Map
+//  map.set('IN', "India")
+//  map.set('USA', "United State Of America")
+//  map.set('FR', "France")
+//  map.set('IN', "India")
+//  console.log(map);
+
+// for in loop
 const myobj = {
     js: 'javascript',
     cpp: 'c++',
@@ -5,7 +26,27 @@ const myobj = {
     swift: "swift for apple"
 }
 for (const key in myobj) {
-   console.log(`${key} shortcut is ${myobj[key]}` );
+//    console.log(`${key} shortcut is ${myobj[key]}` );
  
 }
 const programming = ['js', 'rb', 'py']
+for (const key in programming) {
+    //console.log(programming);
+    //  console.log(programming[key]);
+        
+    }
+
+    
+// As  key is not iteratable so it is not writen in loop like "forin" loop
+//  const map = new Map
+//  map.set('IN', "India")
+//  map.set('USA', "United State Of America")
+//  map.set('FR', "France")
+//  map.set('IN', "India")
+//  for (const key in map) {
+//     console.log(key);
+        
+//     }
+ 
+
+ 
