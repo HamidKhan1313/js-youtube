04a1f3adb72706a9e0a3fcd96525dba5514bb0ca
Hamid Khan <hamidkhantanol@gmail.com>
2024-10-28 16:47:56 +0000

    javascrit code excuted

---
+++ 32-practice/practice.js
@@ -0,0 +1,53 @@
+// let, var, coonst 
+let accountid = 4566765
+const accountEmail = "HamidKhan@gmail.com"
+var accountpassword = 1223334444
+accountEmail = "Waleed@gmail.com"
+console.log([accountid, accountEmail, accountpassword])
+
+// data Types 
+let name = "hamid"
+let age = 23
+let isLoggedIn = true
+
+console.log(isLoggedIn)
+console.log(typeof age)
+console.log(typeof name)
+
+
+// Datatype conversion 
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+let studentId = "F20-0152"
+let stringstudentId = Number(studentId)
+console.log(stringstudentId);
+
+
+// string to number conversion
+let number = 1
+let negativeNumber = -number
+console.log(negativeNumber);
+
+
+let string1 = "Hamid"
+let string2 = "khan"
+let string3 = string1 + string2
+console.log(string3);
+
+console.console.log(typeof string3);
+
+console.log(2 + "2");
+console.log(2 - "2");
+console.log("2" + 2);
+console.log("2" - 2);
+
+
+
+
+
+
+
+
+
